317b83600fa565205bfdbc15b2e4746ee407ad62
ruach <slickwolf@gmail.com>
2017-01-07 17:18:51 +0800

    Add solution to directives exercise

---
+++ vuejs-udemy/12-02-directives-exercise/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+
+                <button v-my-on:click="showAlert" class="btn btn-primary">Show Alert</button>
+                <hr />
+                <div v-my-on:mouseenter="mouseEnter"
+                     v-my-on:mouseleave="mouseLeave"
+                     style="width: 100px; height: 100px; background-color: lightgreen;"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        directives: {
+            'my-on': {
+                bind(el, binding, vnode) {
+                    // if (binding.arg == 'click')
+                    //     el.onclick = binding.value;
+
+                    var type = binding.arg;
+                    var fn = binding.value;
+
+                    el.addEventListener(type, fn);
+                }
+            }
+        },
+        methods: {
+            showAlert() {
+                alert('Hello!');
+            },
+            mouseEnter() {
+                console.log('Mouse enter');
+            },
+            mouseLeave() {
+                console.log('Mouse leave');
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
