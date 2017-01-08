c09a6ebdc881ef376efd539fd13efc9ae6347324
ruach <slickwolf@gmail.com>
2017-01-09 00:24:22 +0800

    Add chapter 15 + integration with axios

---
+++ vuejs-udemy/15-01-connecting-to-server-via-http/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Http</h1>
+            </div>
+
+            <button class="btn btn-primary" @click="testAxios">Test Axios</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    	methods: {
+    		testAxios() {
+    			this.$http.get('http://apiv3.iucnredlist.org/api/v3/version')
+    			.then(function (response) {
+				    console.log(response);
+				})
+				.catch(function (error) {
+				    console.log(error);
+				});
+    		}
+    	}
+    }
+</script>
+
+<style>
+</style>
