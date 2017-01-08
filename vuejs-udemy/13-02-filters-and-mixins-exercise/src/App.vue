f53444459f784be972610bff804d7767eacaee46
ruach <slickwolf@gmail.com>
2017-01-08 15:25:47 +0800

    Add solutions to mixing and filters exercise

--- vuejs-udemy/13-02-filters-and-mixins-exercise/src/App.vue
+++ vuejs-udemy/13-02-filters-and-mixins-exercise/src/App.vue
@@ -5,23 +5,60 @@
                 <h1>Filters & Mixins</h1>
                 <!-- Exercise 1) -->
                 <!-- Build a local Filter which reverses the Text it is applied on -->
+                <h3>Exercise 1</h3>
+                <p>{{ 'Test tesT Test tesT Test tesT' | reverse }}</p>
 
                 <!-- Exercise 2 -->
                 <!-- Build a global Filter which counts the length of a word and it appends it -->
                 <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+                <h3>Exercise 2</h3>
+                <p>{{ 'Test tesT Test tesT Test tesT' | appendCount }}</p>
 
                 <!-- Exercise 3 -->
                 <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
+                <h3>Exercise 3</h3>
+                <p>{{ reversedText }}</p>
+                <p>{{ countAppendedText }}</p>
 
                 <!-- Exercise 4 -->
                 <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
+                <h3>Exercise 4</h3>
+                <p>{{ countAppendedText2 }}</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import { textMixin } from './textMixin.js'
+
     export default {
+        mixins: [ textMixin ],
+        data() {
+            return {
+                text: "         Test        tesssst   w teeeeeeeesssst          teeeeeest Test test         "
+            }
+        },
+        filters: {
+            reverse(text) {
+                return text.trim().split("").reverse().join("");
+            }
+        },
+        computed: {
+            reversedText() {
+                return this.text.trim().split("").reverse().join("");
+            },
+
+            countAppendedText() {
+                var words = this.text.trim().split(/\s+/);
+
+                for (var i = 0; i < words.length; i++) {
+                    words[i] += " (" + words[i].length + ")"
+                }
+
+                return words.join(" ");
+            }
+        }
     }
 </script>
 
--- vuejs-udemy/13-02-filters-and-mixins-exercise/src/main.js
+++ vuejs-udemy/13-02-filters-and-mixins-exercise/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.filter('to-lowercase', function(value) {
-    return value.toLowerCase();
+Vue.filter('appendCount', function(text) {
+	var words = text.trim().split(/\s+/);
+
+	for (var i = 0; i < words.length; i++) {
+		words[i] += " (" + words[i].length + ")"
+	}
+
+    return words.join(" ");
 });
 
 Vue.mixin({
